a3ef6fce58dc6416afa98da63c549d53013967dd
Kelli Sommerdyke & Don Schemmel <student@epicodus.com>
2015-08-04 10:35:51 -0700

    add app & web folders

---
+++ app/app.php
@@ -0,0 +1,12 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+
+    $app = new Silex/Application();
+
+    $app->get("/hello", function() {
+        return "Hello friends!";
+    });
+
+    return $app;
+
+    ?>
